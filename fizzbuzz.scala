cc88192b9fd8bb7531f82085d17aacabb8d64209
ktunprasert <ktunprasert@outlook.com>
2023-03-26 15:10:56 +0100

    fizzbuzz: tabulate

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,6 +1,16 @@
 object FB {
   def main (args: Array[String]) = {
-    for (i <- 1 to 100) {
+    val fb = Array.tabulate(5)(_+1).map { 
+      (i) => i match
+      case i if i % 15 == 0 => "FizzBuzz"
+      case i if i % 5 == 0 => "Buzz"
+      case i if i % 3 == 0 => "Fizz"
+      case _ => i.toString
+    }
+
+    println(fb.mkString("\n"))
+
+    for (i <- 1 to 15) {
       val out = i match {
         case i if i % 15 == 0 => "FizzBuzz"
         case i if i % 5 == 0 => "Buzz"
